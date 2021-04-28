0236321830dd758e749e28af45e7deee704a4dc9
eilam-ashbell <72569792+eilam-ashbell@users.noreply.github.com>
2021-04-28 18:28:05 +0300

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -142,7 +142,7 @@ function firstCalc() {
             plan.pilotY = calcYearly(pFeesEur),
             plan.astronautY = calcYearly(aFeesEur);
     }
-    if (false == feeCheckBox.checked) {
+    if (true == feeCheckBox.checked) {
         plan.explorerM = (plan.explorerM + a),
             plan.pilotM = (plan.pilotM + a),
             plan.astronautM = (plan.astronautM + a),
